dc271b19474b3d2a8fa9220334313f76098439f3
Vivek <vivdave2004@gmail.com>
2025-04-20 16:19:42 +0530

    Refactor code structure for improved readability and maintainability

--- image-class/image-class.py
+++ image-class/image-class.py
@@ -1,10 +1,41 @@
-# Implementing a face recogniser from scratch using encoding. 
-# About the neural network: 3 layer neural network
-# Input Layer: Image size
+# Implementing a face recognising basic non convolutional neural net from scratch with triplet encoding. 
+# About the neural network: 1 input layer 3 hidden layers 1 output neuron
+# Input Layer: Flattened Image
 # Output Layer: Binary(y = 0 for incorrect, y = 1 for correct) 
 # Activation Function: Sigmoid for classifcation between 0 and 1, if output>0.5 then y = 1 else 0
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import pandas as pd
-import os
+import cv2 as cv2
+import os as os
+path = r"D:\deep-learning\image-class\dataset\CelebA FR Triplets\CelebA FR Triplets\images"
+
+triplet_path = r"image-class\dataset\CelebA FR Triplets\CelebA FR Triplets\triplets.csv"
+
+filename = r"image-class/dataset/CelebA FR Triplets/CelebA FR Triplets/images/000015.jpg"
+
+df = pd.DataFrame({"anc", "pos", "neg"})
+relation= pd.read_csv(triplet_path)
+def convert(path, size=(128,128)):
+    processed_data = []
+    for index, rows in relation.iterrows():
+        print(f"Processing Index no: {index}")
+        anc_img,pos_img, neg_img = rows["anchor"],rows["pos"],rows["neg"]
+        anc_path = os.path.join(path, anc_img)
+        pos_path = os.path.join(path, pos_img)
+        neg_path = os.path.join(path, neg_img)
+        anc_image,pos_image, neg_image = cv2.imread(anc_path), cv2.imread(pos_path), cv2.imread(neg_path)
+        anc_image_resized = cv2.resize(anc_image, size)
+        pos_image_resized = cv2.resize(pos_image,size)
+        neg_image_resized = cv2.resize(neg_image, size)
+        new_rows =  (np.array(anc_image_resized).flatten(), np.array(pos_image_resized).flatten(),np.array(neg_image_resized).flatten())
+        processed_data.append(new_rows) 
+    return processed_data
+processed_data = convert(path)
+
+
+            
+
+
